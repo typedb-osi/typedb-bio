29170514855ac8a3dde5ac50e1c80a8642d2a38f
joshua <joshuasend1@gmail.com>
2022-06-10 09:26:33 -0700

    Update to the TypeDB 2.10.0 and client-python 2.9.0

--- migrator.py
+++ migrator.py
@@ -35,7 +35,7 @@ NUM_DR = 10000000  # Total drug to migrate (32k total)
 NUM_INT = 10000000  # Total drug-gene interactions to migrate (42k total)
 NUM_PATH = 10000000  # Total pathway associations to migrate
 NUM_TN = 10000000  # Total TissueNet being migrated
-NUM_PA = 10000000  #  Total Tissues <> Genes to migrate
+NUM_PA = 10000000  # Total Tissues <> Genes to migrate
 NUM_NER = 10000000  # Total number of publications (authors: 110k; journals: 1.7k; publications: 29k)
 NUM_SEM = 10000000  # Total number of rows from Semmed to migrate
 
