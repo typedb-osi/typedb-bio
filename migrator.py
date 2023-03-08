c63c997a95aa604ff69464d23b994525aa21f80e
Shabbir Khan <shabbir@relationrx.com>
2023-03-08 19:16:21 +0000

    Change NUM_SEM

--- migrator.py
+++ migrator.py
@@ -67,8 +67,7 @@ NUM_INT = 10000000  # Total drug-gene interactions to migrate (42k total)
 NUM_PATH = 10000000  # Total pathway associations to migrate
 NUM_TN = 10000000  # Total TissueNet being migrated
 NUM_PA = 10000000  # Total Tissues <> Genes to migrate
-# NUM_SEM = 10000000  # Total number of rows from SemMed to migrate
-NUM_SEM = 10
+NUM_SEM = 10000000  # Total number of rows from SemMed to migrate
 
 start = timer()
 if __name__ == "__main__":
