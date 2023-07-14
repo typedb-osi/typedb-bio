d71955a30cfa7e4f82cd0f032f865bb7b3189f5f
James Whiteside <james.whiteside@vaticle.com>
2023-07-14 15:09:24 +0100

    Fixed tests.

--- tests/loader/semmed/test_semmed_migrator.py
+++ tests/loader/semmed/test_semmed_migrator.py
@@ -32,26 +32,35 @@ def test_migrate_semmed(
     :param monkeypatch: The pytest monkeypatch fixture.
     :type monkeypatch: pytest.MonkeyPatch
     """
+
+    # Check migrate_semmed() works when pulling data
+    # from the NCBI API
+
     with client.session(database, SessionType.DATA) as session:
-        _migrate_semmed = partial(
-            load_semmed,
+        assert load_semmed(
             session=session,
             max_publications=None,
             num_jobs=4,
             batch_size=50,
             cache_dir=cache_dir,
-        )
+        ) is None
 
-        # Check migrate_semmed() works when pulling data
-        # from the NCBI API
-        assert _migrate_semmed() is None
+    # Check migrate_semmed() works when pulling data
+    # from the cache
 
-        # Check migrate_semmed() works when pulling data
-        # from the cache
+    initialise_database(client, database, True)
+
+    with client.session(database, SessionType.DATA) as session:
         with monkeypatch.context() as m:
             # Monkeypatch the requests.get() function to raise an exception.
             # This will not cause a problem if the data is read from the cache,
             # before trying to be pulled from the NCBI API.
             m.setattr(requests, "get", _raise_exception)
-            initialise_database(client, database, True)
-            assert _migrate_semmed() is None
+
+            assert load_semmed(
+                session=session,
+                max_publications=None,
+                num_jobs=4,
+                batch_size=50,
+                cache_dir=cache_dir,
+            ) is None
